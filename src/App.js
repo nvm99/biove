d1ab0015786056d35d0076ff553a2db037a414be
Nvm <vominhbinto@gmail.com>
2021-03-30 15:46:43 +0700

    push lai

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React, { Component } from 'react';
-import './App.css';
-import Header from "./Components/Header"
-import Slider from "./Components/Slider"
-import Introduction from "./Components/Introduction"
+import React, { Component } from "react";
+import "./App.css";
+import Header from "./Components/Header";
+import Slider from "./Components/Slider";
+import Introduction from "./Components/Introduction";
 import Heatmap from "./Components/Heatmap";
 import Testimonial from "./Components/Testimonial";
 import Footer from "./Components/Footer";
-import Login from "./pages/Login"
-import Signup from "./pages/Signup"
-import Navbar from "./Components/Navbar"
-import Profile from "./pages/Profile"
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Navbar from "./Components/Navbar";
+import Profile from "./pages/Profile";
 export default class componentName extends Component {
   constructor(props) {
     super(props);
-    this.state = { page: 'home' };
+    this.state = { page: "home" };
   }
   setPage = (page) => {
-    this.setState({ page: page })
-  }
+    this.setState({ page: page });
+  };
   renderPage() {
     switch (this.state.page) {
-      case 'home':
+      case "home":
         return (
           <>
             <Profile></Profile>
@@ -29,22 +29,20 @@ export default class componentName extends Component {
             <Introduction></Introduction>
             <Heatmap></Heatmap>
             <Testimonial></Testimonial> */}
-
           </>
-        )
-      case 'login':
+        );
+      case "login":
         return (
           <>
             <Login setPage={this.setPage} />
-            
           </>
-        )
-      case 'signup':
+        );
+      case "signup":
         return (
           <>
             <Signup setPage={this.setPage} />
           </>
-        )
+        );
       default:
         <>
           <Header></Header>
@@ -58,20 +56,25 @@ export default class componentName extends Component {
   render() {
     return (
       <div className="App">
-        <div style={Styles.navbarContainer}>
-          <Navbar  setPage={this.setPage}/>
+        <div
+          style={
+            // this.state.page == "home"
+            //   ? { width: "100%", position: "absolute", zIndex: "10" }
+            //   : Styles.navbarContainer
+            Styles.navbarContainer
+          }
+        >
+          <Navbar setPage={this.setPage} />
         </div>
         {this.renderPage()}
         <Footer />
       </div>
     );
   }
-
 }
 const Styles = {
   navbarContainer: {
     width: "100%",
     backgroundColor: "#3A3232",
-  }
-}
-
+  },
+};
