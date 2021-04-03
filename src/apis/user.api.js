7d34bdd2a734dafdabcef6e2b197d73fd898ec2f
phutucthinh0 <31724729+phutucthinh0@users.noreply.github.com>
2021-04-03 20:50:28 +0700

    thinh

--- src/apis/user.api.js
+++ src/apis/user.api.js
@@ -1,5 +1,5 @@
-// const url = "https://api.biove.life/";
-const url = "http://localhost:2000/";
+const url = "https://api.biove.life/";
+// const url = "http://localhost:2000/";
 export async function profile() {
     const response = await fetch(url+"user/profile", {
         method: 'GET',
